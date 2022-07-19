53ea86475c3ac782ea14e8265f435f4c1d3c55ba
LuisEJaar <luisjaarcoding@gmail.com>
2022-07-19 16:57:44 -0500

    updated file hosting location

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function About () {
     return (
-        <div className="pt-1 pb-1 card-body">
+        <div className="pt-1 pb-3 card-body">
             <h3 className="card-title">About</h3>
             <p className="card-text">I am a Stack Developer who has transitioned from the aviation industry in search of fun new tools to work with and on. I enjoy making rich and accessible tools geared towards meeting peoples needs.</p>
             <h3 className="card-title">Interests</h3>
